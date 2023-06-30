f9bcc45f70fbcfe6fe3d78957af656a7a7dd074f
slmnkankal <slmnknkl@gmail.com>
2023-06-30 16:58:45 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <Container>
+          <Row>
+            <Col className="text-center">
+              Copyright &copy; E-Commerce
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
